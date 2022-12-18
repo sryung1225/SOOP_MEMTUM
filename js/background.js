5f9bec088e5afed5387eb84bcabd0037426484f1
sryung <sryung1225@gmail.com>
2022-12-19 00:09:03 +0900

    Background

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+//console.log(chosenImage);
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+//console.log(bgImage);
+document.body.appendChild(bgImage);
