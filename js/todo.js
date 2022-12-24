2a9c3f53db1f3d8e9769073cfddcafe89c2c4202
sryung <sryung1225@gmail.com>
2022-12-24 16:18:40 +0900

    ToDos : Saving ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,15 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const toDos = []; // todo를 저장할 배열
+
+function saveToDos(){
+  //todo를 브라우저에 저장하기 위해 localStorage 사용.
+  //대신 localStorage는 배열을 저장할 수 없기 때문에 배열처럼 보이도록 변환해줌
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(e){
-  // console.log("click!");
-  // console.log(e.target);
-  // console.log(e.target.parentElement);
   const li = e.target.parentElement;
   li.remove();
 }
@@ -24,6 +29,8 @@ function handleToDoSubmit(e){
   e.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
