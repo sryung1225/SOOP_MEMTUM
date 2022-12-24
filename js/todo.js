8b784585e0615e708e8a19e6f74925a39fcb12ec
sryung <sryung1225@gmail.com>
2022-12-24 15:10:05 +0900

    ToDos : Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,18 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo){
+  //console.log("I will paint ", newToDo);
+  const listItem = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newToDo;
+  toDoList.appendChild(listItem).appendChild(span);
+}
+
 function handleToDoSubmit(e){
   e.preventDefault();
-  //console.log(toDoInput.value);
-  // 새 변수에 value값 저장하고 input에 입력된 건 육안에서 지우기
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newToDo);
+  paintToDo(newToDo);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
