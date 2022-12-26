0936bf3c60f265ce35a9874a7c2417df2b7b61c3
sryung <sryung1225@gmail.com>
2022-12-26 23:32:32 +0900

    ToDos : Loading ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,13 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = []; // todo를 저장할 배열
+const TODOS_KEY = "todos";
+let toDos = []; // todo를 저장할 배열
 
 function saveToDos(){
   //todo를 브라우저에 저장하기 위해 localStorage 사용.
   //대신 localStorage는 배열을 저장할 수 없기 때문에 배열처럼 보이도록 변환해줌
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(e){
@@ -31,6 +32,14 @@ function handleToDoSubmit(e){
   toDoInput.value = "";
   toDos.push(newToDo);
   paintToDo(newToDo);
-  saveToDos();
+  saveToDos(); // 등록된 todos는 나중에도 사용할 수 있도록 저장
 }
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+} // 이전에 등록한 todos가 있다면 가져와서 돔 그려주기
