01bd1d657850d1870c11bba770fb70bb30a6a7bd
sryung <sryung1225@gmail.com>
2023-04-28 18:32:46 +0900

    [feature] 최초 로그인 화면 구현

--- js/greetings.js
+++ js/greetings.js
@@ -1,26 +1,31 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const loginBoard = document.querySelector(".loginboard");
+const loginForm = loginBoard.querySelector("#login-form");
+const loginInput = loginBoard.querySelector("#login-form input");
+const dashBoard = document.querySelector(".dashboard");
+const greeting = dashBoard.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){
+function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-	const username = loginInput.value;
+  const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
 
-function paintGreetings(username){
+function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  loginBoard.classList.add(HIDDEN_CLASSNAME);
+  dashBoard.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null){
+if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
