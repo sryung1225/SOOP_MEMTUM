7ba8dd43c2b4c72a9d7afada783cd5e7f569d724
sryung <sryung1225@gmail.com>
2023-04-29 01:32:49 +0900

    [feature] 대시보드 화면 1차 레이아웃 & 날짜 추가

--- js/greetings.js
+++ js/greetings.js
@@ -1,24 +1,22 @@
 const loginBoard = document.querySelector(".loginboard");
 const loginForm = loginBoard.querySelector("#login-form");
-const loginInput = loginBoard.querySelector("#login-form input");
+const loginInput = loginForm.querySelector("input");
 const dashBoard = document.querySelector(".dashboard");
-const greeting = dashBoard.querySelector("#greeting");
+const greeting = dashBoard.querySelector(".greeting .name");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginBoard.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello, ${username}!`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-
+  greeting.innerText = username;
   loginBoard.classList.add(HIDDEN_CLASSNAME);
   dashBoard.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -26,7 +24,7 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginBoard.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
